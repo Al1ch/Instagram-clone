5627e5b9f4741a1bab53bc1c88a79dd975c0dd59
Alain <alain.chea@efrei.net>
2023-07-27 15:33:51 +0200

    fix(architecture): consistency with RSC mindset

---
+++ components/PublicationSection.tsx
@@ -0,0 +1,49 @@
+'use client';
+import React, { use, useEffect, useState } from 'react';
+import Tab from './Tab';
+import PublicationForm from './PublicationForm';
+import Publication from './Publication';
+import { User, Post } from '@/models.types';
+import axios from 'axios';
+
+type Props = {
+	profilePic?: string | null;
+	id?: number;
+	name?: string;
+	posts: Post[];
+};
+
+const PublicationSection = ({ profilePic, name, id: userId, posts: postsInit }: Props) => {
+	const [posts, setPosts] = useState<Post[]>(postsInit);
+
+	const handleSubmit = async () => {
+		try {
+			const res = await axios.get<Post[]>('/api/posts', { params: { author: userId } });
+			setPosts(res.data);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	return (
+		<div className=" w-full max-w-2xl flex flex-col items-center  justify-center px-8 gap-4  ">
+			<div className="w-full flex justify-center items-center gap-16 ">
+				<Tab label="Publications" />
+			</div>
+			<div className="w-full bg-white ">
+				<PublicationForm image={profilePic ?? ''} authorId={userId} onSubmit={handleSubmit} />
+			</div>
+
+			{posts.map((post) => (
+				<Publication
+					key={post.id}
+					url={profilePic ?? ''}
+					name={name ?? ''}
+					content={post.content ?? ''}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default PublicationSection;
